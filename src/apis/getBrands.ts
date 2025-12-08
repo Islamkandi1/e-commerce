cad8637304efd7e27f185cf8f129264a3430f7c6
Islamkandi1 <eslamkandil097@gmail.com>
2025-12-08 15:52:49 +0200

    hadle env URL

--- src/apis/getBrands.ts
+++ src/apis/getBrands.ts
@@ -1,6 +1,6 @@
 "use server";
 export default async function getBrands() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/brands`);
+    const res = await fetch(`${process.env.URL}/api/brands`);
     if (!res.ok) throw new Error("Failed to fetch brands ");
     const data = await res.json();
     return data;
--- src/apis/getSubCategory.ts
+++ src/apis/getSubCategory.ts
@@ -1,6 +1,6 @@
 "use server";
 export default async function getSubcategory() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/dressStyle`);
+    const res = await fetch(`${process.env.URL}/api/dressStyle`);
     if (!res.ok) throw new Error("Failed to fetch sucategory ");
     const data = await res.json();
     return data;

474f65e9656412d92a6a3b39077aaed14bd520b5
Islamkandi1 <eslamkandil097@gmail.com>
2025-12-09 20:17:21 +0200

    handle cashing orders

--- src/app/(page)/orders/page.tsx
+++ src/app/(page)/orders/page.tsx
@@ -1,25 +1,15 @@
-import getAllOrder from '@/apis/getAllOrders'
 import MyOrders from '@/app/_components/myOrders/MyOrders'
 import React from 'react'
-import img from "./../../../assets/images/noData.jpg"
-import Image from 'next/image'
 import { Metadata } from 'next';
 export const metadata: Metadata = {
     title: "My Order",
     description: "Order Summary Page",
 };
 const Order = async () => {
-    const { data } = await getAllOrder()
-    if (data!.length < 1) {
-        return <main className="flex justify-center items-center min-h-screen overflow-hidden">
-            <figure className=" pt-12 md:w-[50%] ">
-            <Image src={img} alt="no data to display" className="w-full pt-3 mx-auto" />
-        </figure>
-        </main> 
-    }
+
     return (
         <>
-            <MyOrders data={data} />
+            <MyOrders  />
 
         </>
     )
--- src/app/_components/myOrders/MyOrders.tsx
+++ src/app/_components/myOrders/MyOrders.tsx
@@ -1,15 +1,30 @@
+"use client"
 import React from 'react'
 import OrderTable from '../orderTable/OrderTable'
-import {  OrderProducts } from '@/types/order.type'
-
-const MyOrders = ({data}:{data:OrderProducts[]|null}) => {
+import { useQuery } from '@tanstack/react-query'
+import getAllOrder from '@/apis/getAllOrders'
+import img from "./../../../assets/images/noData.jpg"
+import Image from 'next/image'
+const MyOrders = () => {
+  const { data } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getAllOrder
+  })
+  
   return (
-            <main className='pt-12'>
-                <section className="pt-12 mx-2 md:mx-15 min-h-screen">
-                    <h2 className='text-[1.7rem] font-bold text-main text-center mb-4  tracking-wide uppercase'>all orders</h2>
-                    {data?.map((product) => <OrderTable key={product.id} product={product} />)}
-                </section>
-            </main>
+    <>
+    {(data?.data?.length ?? 0) < 1  && <section className="flex justify-center items-center min-h-screen overflow-hidden">
+      <figure className=" pt-12 md:w-[50%] ">
+        <Image src={img} alt="no data to display" className="w-full pt-3 mx-auto" />
+      </figure>
+    </section> }
+    <main className='pt-12'>
+      <section className="pt-12 mx-2 md:mx-15 min-h-screen">
+        <h2 className='text-[1.7rem] font-bold text-main text-center mb-4  tracking-wide uppercase'>all orders</h2>
+        {data?.data!.map((product) => <OrderTable key={product.id} product={product} />)}
+      </section>
+    </main>
+    </>
   )
 }
 
